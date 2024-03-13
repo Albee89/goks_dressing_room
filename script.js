5e96541399703a9db57c3b3a78085179cc3714a3
Alex B <137462408+Albee89@users.noreply.github.com>
2024-03-13 23:47:44 +1000

    Update script.js
    I am giving Gok a beautiful rainbow firework rendered behind him background because I wanted to play with Canvas and also...he deserves it

--- script.js
+++ script.js
@@ -1,42 +1,109 @@
-//controls
-const hatcheck = document.querySelector("#hat");
-const glassescheck = document.querySelector("#eyeglasses");
-const tiecheck = document.querySelector("#tie");
-//accessories
-const hat = document.querySelector(".hat");
-const glasses = document.querySelector(".glasses");
-const tie = document.querySelector(".tie");
+<!DOCTYPE html>
+<html lang="en">
 
-//Reveal Hat
-hatcheck.addEventListener("change", hatfun);
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="styles.css">
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/fabric.js/4.5.0/fabric.min.js"></script>
+  
+  <title>Gok's Dressing Room</title>
+</head>
 
-function hatfun() {
-  if (hatcheck.checked == true) {
-    hat.style.bottom = "165px";
-  } else {
-    hat.style.bottom = "400px";
-  }
-}
+<body>
 
-//Reveal Eyeglasses
-glassescheck.addEventListener("change", glassesfun);
+<canvas id="c1" width="400" height="400"></canvas>
+<canvas id="c2" width="400" height="400"></canvas>
+  
 
-function glassesfun() {
-  if (glassescheck.checked == true) {
-    glasses.style.right = "50%";
-  } else {
-    glasses.style.right = "-50%";
-  }
-}
+  <div class="container">
+    <div class="wrapper">
+      <div class="detective-hat">
+        <div class="flap"></div>
+      </div>
 
-//Reveal Tie
-tiecheck.addEventListener("change", tiefun);
+      <div class="judge-wig">
+        <div class="middle-part"></div>
+        <div class="curl"></div>
+        <div class="curl" style="left: auto; right: 0;"></div>
+      </div>
 
-function tiefun() {
-  if (tiecheck.checked == true) {
-    tie.style.bottom = "10px";
-  } else {
-    tie.style.bottom = "-80px";
-  }
-}
+      <div class="ears">
+        <div class="ear"></div>
+        <div class="ear"></div>
+      </div>
+      <div class="head">
+        <div class="mustaches">
+          <div class="mus">
+            <div class="mustach"></div>
+            <div class="mustach"></div>
+            <div class="mustach"></div>
+            <div class="lower-head"></div>
+          </div>
+          <div class="mus">
+            <div class="mustach"></div>
+            <div class="mustach"></div>
+            <div class="mustach"></div>
+          </div>
+        </div>
+        <div class="eyes">
+          <div class="eye"></div>
+          <div class="eye"></div>
+        </div>
+        <div class="nose">
+          <div class="shape"></div>
+        </div>
+        <div class="mouth">^</div>
+      </div>
+      <div class="body"></div>
+      <div class="pads">
+        <div class="pad"></div>
+        <div class="pad"></div>
+      </div>
+      <div class="hat">
+        <div class="bottom"></div>
+      </div>
+      <div class="glasses">
+        <div class="glass"></div>
+        <div class="glass"></div>
+      </div>
+      <div class="tie"></div>
+    </div>
+    <div class="controls">
+      <input type="checkbox" id="eyeglasses">
+      <input type="checkbox" id="hat">
+      <input type="checkbox" id="tie">
+      <label class="glassescontrol" data-text="eyeglasses" for="eyeglasses">
+        <div class="glassesicon">
+          <div class="glassicon"></div>
+          <div class="glassicon"></div>
+        </div>
+      </label>
+      <label data-text="Hat" class="hatcontrol" for="hat">
+        <div class="haticon">
+          <div class="bottom">
+          </div>
+        </div>
+      </label>
+      <label data-text="Tie" class="tiecontrol" for="tie">
+        <div class="tieicon"></div>
+      </label>
+      <label data-text="Detective Hat" class="detective-hat-control" for="detective-hat-control">
+        <div class="detective-haticon">
+          <div class="bottom"></div>
+        </div>
+      </label>
+      
+      <label data-text="Judge Wig" class="judge-wig-control" for="judge-wig-control">
+        <div class="judge-wigicon">
+          <div class="bottom"></div>
+        </div>
+      </label>
+    </div>
+  </div>
 
+  <script src="script.js"></script>
+  
+</body>
+
+</html>
